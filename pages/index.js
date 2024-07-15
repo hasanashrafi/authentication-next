caedcfbd97c35eee20fd8cbfa2fba848dd154259
Hasan Ashrafi <yasashrafi@gmail.com>
2024-07-15 19:55:39 +0330

    added content div

--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,16 @@
 export default function Home() {
   return (
     <main
-      className={`min-h-screen justify-between p-5 `}
+      className={`min-h-screen bg-violet-100 justify-between p-5 `}
     >
-      <div className="shadow-xl rounded-xl p-4   w-full items-center justify-between font-mono text-sm ">
-        <h2 className="flex w-fit text-4xl text-indigo-700 text-center mx-auto font-semibold">
+      <div className="shadow-xl rounded-xl p-4 w-full items-center justify-between font-mono text-sm ">
+        <h2 className="flex w-fit text-4xl text-indigo-900 text-center mx-auto font-semibold">
           Authentication & Authorization
         </h2>
       
+      </div>
+      <div>
+        
       </div>
     </main>
   );
