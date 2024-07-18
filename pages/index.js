6d317c8fa177c0c6977a3edc3d91f472a09b399c
Hasan Ashrafi <yasashrafi@gmail.com>
2024-07-18 19:01:52 +0330

    edit styles

--- pages/index.js
+++ pages/index.js
@@ -4,20 +4,20 @@ import { IoIosCreate } from "react-icons/io";
 export default function Home() {
   return (
     <main
-      className={`min-h-screen bg-violet-100 justify-between p-5 `}
+      className={`min-h-screen min-w-screen bg-violet-100  `}
     >
-      <div className="flex shadow-xl rounded-xl p-4 w-full items-center justify-between  ">
-        <h2 className="flex w-fit text-4xl text-indigo-900 text-center mx-auto font-semibold">
-          Authentication & Authorization
+      <div className="flex shadow-xl  p-4  items-center justify-between ">
+        <h2 className="flex w-fit text-wrap text-2xl  text-indigo-900 text-center  font-semibold">
+         Skin Care
         </h2>
 
         <div className="flex items-center">
-          <button className="flex items-center justify-between p-3 m-4 border bg-sky-600 rounded-lg mr-2 text-white  text-xl border-none shadow-xl">
+          <button className="flex items-center justify-between p-1.5 m-2 border bg-sky-600 rounded-lg mr-2 text-white  text-md border-none shadow-xl">
             <IoIosCreate className="text-2xl mr-2" />
             <Link href='/signup'> SignUp</Link>
           </button>
 
-          <button className="flex items-center justify-between p-3 m-4 border bg-indigo-600 rounded-lg text-white  text-xl border-none shadow-xl">
+          <button className="flex items-center justify-between p-1.5 m-2 border bg-indigo-600 rounded-lg text-white  text-md border-none shadow-xl">
             <FaSignInAlt className="text-2xl mr-2" /> <Link href='/signin'> SignIn</Link>
           </button>
         </div>
