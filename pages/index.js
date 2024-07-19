34bbf4cea439dc3f94daa8fe337d1a8d660f7e68
Hasan Ashrafi <yasashrafi@gmail.com>
2024-07-19 19:43:44 +0330

    added dashbaord button and edit styles

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { FaSignInAlt } from "react-icons/fa";
 import { IoIosCreate } from "react-icons/io";
+import { LuLayoutDashboard } from "react-icons/lu";
 export default function Home() {
   return (
     <main
@@ -11,14 +12,19 @@ export default function Home() {
          Skin Care
         </h2>
 
-        <div className="flex items-center">
-          <button className="flex items-center justify-between p-1.5 m-2 border bg-sky-600 rounded-lg mr-2 text-white  text-md border-none shadow-xl">
-            <IoIosCreate className="text-2xl mr-2" />
-            <Link href='/signup'> SignUp</Link>
+        <div className="flex flex-wrap items-center justify-end">
+          <button className="flex  w-240 justify-center  items-center p-1.5 m-1 border bg-sky-600 rounded-lg text-white  text-md border-none shadow-xl">
+            <IoIosCreate className="sm:text-2xl mr-2" />
+            <Link href='/signup' className="text-sm"> SignUp</Link>
           </button>
 
-          <button className="flex items-center justify-between p-1.5 m-2 border bg-indigo-600 rounded-lg text-white  text-md border-none shadow-xl">
-            <FaSignInAlt className="text-2xl mr-2" /> <Link href='/signin'> SignIn</Link>
+          <button className="flex  w-20 justify-center  items-center p-1.5 m-1 border bg-indigo-600 rounded-lg text-white  text-md border-none shadow-xl">
+            <FaSignInAlt className="sm:text-2xl mr-2" />
+             <Link href='/signin' className="text-sm"> SignIn</Link>
+          </button>
+           <button className="flex w-240 justify-center   items-center p-1.5 m-1 border bg-indigo-600 rounded-lg text-white  text-md border-none shadow-xl">
+            <LuLayoutDashboard className="sm:text-2xl mr-2" />
+             <Link href='/dashboard' className="text-sm"> Dashboard</Link>
           </button>
         </div>
       </div>
