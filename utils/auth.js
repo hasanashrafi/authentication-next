66fb89752982d0779710ee006c29c8dba1a969f8
Hasan Ashrafi <yasashrafi@gmail.com>
2024-07-21 18:37:15 +0330

    added comment

--- utils/auth.js
+++ utils/auth.js
@@ -1,5 +1,17 @@
+/**
+ * The code includes functions for hashing passwords, verifying passwords, and verifying tokens with a
+ * secret key.
+ * @param password - The `password` parameter is a string that represents the user's password. It is
+ * used as input to hash the password for storage and verification.
+ * @returns The `hashPassword` function returns the hashed password after hashing the input password
+ * with a cost factor of 12. The `verifyPassword` function returns a boolean value indicating whether
+ * the input password matches the hashed password. The `verifyToken` function returns an object with
+ * the email extracted from the verified token if successful, otherwise it returns false.
+ */
+
 import { compare, hash } from "bcryptjs";
 import { verify } from "jsonwebtoken";
+
 
 async function hashPassword(password) {
     const hashedPassword = await hash(password, 12)
