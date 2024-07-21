aef223af7869882bc941d3ca243224a94f82c344
Hasan Ashrafi <yasashrafi@gmail.com>
2024-07-21 18:34:32 +0330

    create dashboard route

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,11 +1,28 @@
+import { verifyToken } from '@/utils/auth'
+import { redirect } from 'next/dist/server/api-utils'
 import React from 'react'
 
 function Dashboard() {
     return (
         <div className='min-h-screen p-5 '>
-        Dashboard
+            Dashboard
         </div>
     )
 }
 
 export default Dashboard
+
+
+export async function getServerSideProps(context) {
+    const { token } = context.req.cookies
+    const secretKey = process.env.SECRET_KEY
+
+    const result = verifyToken(token, secretKey)
+    if (!result)
+        return {
+            redirect: { destination: "/signin", permanent: false },
+        }
+    return {
+        props: { result }
+    }
+}
