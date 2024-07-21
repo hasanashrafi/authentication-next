6ff8774de0a4f175e67554b2c62e5bb40482675c
Hasan Ashrafi <yasashrafi@gmail.com>
2024-07-21 18:35:38 +0330

    fetch data with useeffect

--- pages/signin.js
+++ pages/signin.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useRouter } from 'next/router';
 
 import { FaRegUser } from "react-icons/fa";
@@ -17,6 +17,16 @@ function SignIn() {
   const [user, setUser] = useState({})
 
   const router = useRouter()
+
+  useEffect(() => {
+    fetch("/api/user")
+      .then((res) => (res).json())
+      .then((data) => {
+        if (data.status === "success") window.location.href = "/dashboard"
+
+        
+      })
+  }, [])
 
   const addHandler = async () => {
     const res = await fetch("/api/auth/signin", {
@@ -43,7 +53,7 @@ function SignIn() {
         <h3 className="flex justify-center gap-2 mb-4 text-center text-3xl font-semibold font- text-indigo-900">
           <FaRegUser />  SignIn
         </h3>
-      <div >{ user.status && <h3 className={`w-full my-1 text-center text-sm p-2 rounded text-white ${user.status === "success" ? "bg-green-400" : "bg-red-400"}`}>{user.message}</h3>}</div>
+        <div >{user.status && <h3 className={`w-full my-1 text-center text-sm p-2 rounded text-white ${user.status === "success" ? "bg-green-400" : "bg-red-400"}`}>{user.message}</h3>}</div>
 
         <div className='w-full flex justify-center items-center mx-auto'>
           <MdOutlineAlternateEmail className='text-indigo-900 text-4xl' />
@@ -68,7 +78,7 @@ function SignIn() {
         </div>
 
         <button
-        onClick={addHandler}
+          onClick={addHandler}
           className='mx-auto w-full sm:w-full p-2 bg-blue-600 text-white rounded-md text-md sm:text-xl m-2 mt-5  hover:text-blue-700 hover:bg-white transition-all ease-in-out'>
           <Link href='/signin'>
             LogIn
